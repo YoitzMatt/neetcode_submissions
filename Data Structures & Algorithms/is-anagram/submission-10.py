2a6a62980626f6eb9e0dcd8fb72fa40da8cf9a20
Matt <157982132+YoitzMatt@users.noreply.github.com>
2026-09-15 19:19:31 -0500

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        mp1 = {}
+        mp2 = {}
+        for c in s:
+            if c not in mp1:
+                mp1[c] = 1
+            else:
+                mp1[c] += 1
+
+        for c in t:
+            if c not in mp2:
+                mp2[c] = 1
+            else:
+                mp2[c] += 1
+
+        return mp1 == mp2
